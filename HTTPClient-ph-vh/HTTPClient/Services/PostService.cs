706bc767ecf73da0e83582d5c33b76c7519cdcf1
phsantoip <pedrosantodsilva@gmail.com>
2024-09-23 01:21:26 -0300

    aulas pam-I

---
+++ HTTPClient-ph-vh/HTTPClient/Services/PostService.cs
@@ -0,0 +1,48 @@
+﻿using HTTPClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HTTPClient.Services
+{
+    internal class PostService
+    {
+        private HttpClient httpsClient;
+        private Post post;
+        private List<Post> posts;
+        private JsonSerializerOptions jsonSerializerOptions;
+        public PostService()
+        {
+            httpsClient = new HttpClient();
+            jsonSerializerOptions = new JsonSerializerOptions() { 
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true,
+            };
+        }
+        public async Task<List<Post>>getPosts()
+        {
+            Uri uri = new Uri("http://jsonplaceholder.typicode.com/posts");
+            List<Post> items= new List<Post>();
+
+            try
+            {
+                HttpResponseMessage response = await httpsClient.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    items = JsonSerializer.Deserialize<List<Post>>(content, jsonSerializerOptions);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return items;
+        }
+    }
+}
---
+++ HTTPClient-ph-vh/HTTPClient/ViewModels/PostViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using HTTPClient.Models;
+using HTTPClient.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HTTPClient.ViewModels
+{
+    public partial class PostViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        List<Post> posts;
+
+        private ICommand getPostsCommad { get; }
+
+
+        public async void getPosts()
+        {
+           PostService postService = new PostService();
+           posts = await postService.getPosts();
+        }
+    }
+}
